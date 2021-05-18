ab52b544a2ff124622a8af25289dc306d66c27f4
YuanZheng Hu (Brad) <bhu078@uottawa.ca>
2021-05-18 15:52:22 +0000

    updt

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+def qs(arr):
+    print(arr and (qs([i for i in arr[1:] if i< arr[0]])) + \
+         [arr[0]] + \
+         (qs([i for i in arr[1:] if i >= arr[0]])))
+
+    return arr and (qs([i for i in arr[1:] if i< arr[0]])) + \
+         [arr[0]] + \
+         (qs([i for i in arr[1:] if i >= arr[0]]))
 
 
-A = [1,0,1,0,1]
 
-for j,x in enumerate(A):
-    print(j,x)
+# print(qs([1,2,41,2,9,12]))
+a = []
+print(a[2:])
